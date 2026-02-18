e1758b3e6e7a51d54e7e03967e6d2300204362ea
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-02-18 15:27:05 -0500

    Changing Open & Close Buttons

--- scripts/script.js
+++ scripts/script.js
@@ -66,6 +66,9 @@ navLinks.forEach(link => {
     });
 });
 
+document.getElementById("openButton").addEventListener("click", openNav);
+document.getElementById("closeButton").addEventListener("click", closeNav);
+
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
