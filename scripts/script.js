257788b30b1bec703a914586821b97d8c383b7cc
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-02-18 17:29:24 -0500

    Reordered Code

--- scripts/script.js
+++ scripts/script.js
@@ -66,15 +66,6 @@ navLinks.forEach(link => {
     });
 });
 
-// Get the open and close buttons for the side navigation
-var openButton = document.getElementById("openButton");
-var closeButton = document.getElementById("closeButton");
-var sideNav = document.getElementById("mySidenav");
-
-// Event listeners for opening and closing the side navigation
-openButton.addEventListener("click", openNav);
-closeButton.addEventListener("click", closeNav);
-
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   alert("Opened side navigation");  
@@ -88,3 +79,12 @@ function closeNav() {
   sideNav.style.width = "0";
   document.body.style.backgroundColor = "white";
 }
+
+// Get the open and close buttons for the side navigation
+var openButton = document.getElementById("openButton");
+var closeButton = document.getElementById("closeButton");
+var sideNav = document.getElementById("mySidenav");
+
+// Event listeners for opening and closing the side navigation
+openButton.addEventListener("click", openNav);
+closeButton.addEventListener("click", closeNav);
