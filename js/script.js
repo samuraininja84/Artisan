7ed34ccf7b5120d2b8bf54b4a3d2fae5b117bcae
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-02-18 18:37:17 -0500

    Added Main ID For Stylesheet

--- js/script.js
+++ js/script.js
@@ -48,18 +48,21 @@ navLinks.forEach(link => {
 var toggleButton = document.getElementById("toggleButton");
 var closeButton = document.getElementById("closeButton");
 var sideNav = document.getElementById("mySidenav");
+var mainContent = document.getElementById("main");
 
 const size = 250; 
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     sideNav.style.width = size + "px";
+    mainContent.style.marginLeft = size + "px";
     document.body.style.backgroundColor = "rgba(0, 0, 0, 0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
     sideNav.style.width = "0";
+    mainContent.style.marginLeft = "0";
     document.body.style.backgroundColor = "white";
 }
 
