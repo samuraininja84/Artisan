3d2793622c07e53289ca2f210f9ffeb535e90024
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-02-18 16:12:21 -0500

    Updated Side Bar Styling

--- index.md
+++ index.md
@@ -15,11 +15,13 @@ layout: default
 
 <!-- Side Navigation Bar -->
 <div id="mySidenav" class="sidenav">
-    <button type="button" id="closeButton" class="button">&times;</button>
-    <a href="#Home" class="nav-link">Home</a>
-    <a href="#About Me" class="nav-link">About Me</a>
-    <a href="#Games" class="nav-link">Games</a>
-    <a href="#Tools" class="nav-link">Tools</a>
+    <ul>
+        <li><button type="button" id="closeButton" class="button">&times;</button></li>    
+        <li><a href="#Home" class="nav-link">Home</a></li>
+        <li><a href="#About Me" class="nav-link">About Me</a></li>
+        <li><a href="#Games" class="nav-link">Games</a></li>
+        <li><a href="#Tools" class="nav-link">Tools</a></li>
+    </ul>
 </div>
 
 <section id="Home" class="page-section"></section>
--- scripts/script.js
+++ scripts/script.js
@@ -74,19 +74,19 @@ const closeButton = document.getElementById("closeButton");
 openButton.addEventListener("click", openNav);
 closeButton.addEventListener("click", closeNav);
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+/* Set the width of the side navigation to 250px and the right margin of the page content to 250px and add a black background color to body */
 function openNav() {
   alert("Opened side navigation");  
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("main").style.marginRight = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+/* Set the width of the side navigation to 0 and the right margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   alert("Closed side navigation");  
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("main").style.marginRight = "0";
   document.body.style.backgroundColor = "white";
 }
 
