7e60af1d769a30b62ff0d4fc33831ea7d3fc741f
Samuraininja84 <158503209+samuraininja84@users.noreply.github.com>
2026-02-18 15:34:27 -0500

    Updated The JS

--- scripts/script.js
+++ scripts/script.js
@@ -66,9 +66,13 @@ navLinks.forEach(link => {
     });
 });
 
+// Get the open and close buttons for the side navigation
+const openButton = document.getElementById("openButton");
+const closeButton = document.getElementById("closeButton");
+
 // Event listeners for opening and closing the side navigation
-document.getElementById("openButton").addEventListener("click", openNav);
-document.getElementById("closeButton").addEventListener("click", closeNav);
+openButton.addEventListener("click", openNav);
+closeButton.addEventListener("click", closeNav);
 
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
